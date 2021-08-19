782e0af4aaa90bfe81fdefd2a77d9f61df491d0e
Moritz Walter <moritz.walter@the-systems.eu>
2021-08-19 23:21:54 +0200

    remove beta

--- WebSite/config/version.php
+++ WebSite/config/version.php
@@ -1,5 +1,5 @@
 <?php
 $version = json_encode(array(
-    "version" => "1.8_beta3",
+    "version" => "1.8",
     "version_url" => "https://project.the-systems.eu/api/resource/?resourceid=1&type=allinfos"
 ));
